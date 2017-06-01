81b5ae4fecc20a63290c8bf1c830df24dd3ade75
rodolfoom <rodolfoom@hotmail.com>
2017-06-01 18:15:32 -0500

    modificando la prueba

--- src/rom/proyecto/prueba/PruebaEjemplo.java
+++ src/rom/proyecto/prueba/PruebaEjemplo.java
@@ -4,7 +4,8 @@ public class PruebaEjemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Solo un ejemplo de prueba de clase básica");
+		System.out.println("Solo un ejemplo de prueba de clase básica 1");
+		System.out.println("Solo un ejemplo de prueba de clase básica 2");
 	}
 
 }
