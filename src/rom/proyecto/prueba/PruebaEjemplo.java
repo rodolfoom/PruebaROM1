0f6bb60dd472f506cfed5eaed2f354a70e039b11
rodolfoom <rodolfoom@hotmail.com>
2017-06-01 18:09:28 -0500

    Anexando Clase de Prueba Java

---
+++ src/rom/proyecto/prueba/PruebaEjemplo.java
@@ -0,0 +1,10 @@
+package rom.proyecto.prueba;
+
+public class PruebaEjemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Solo un ejemplo de prueba de clase básica");
+	}
+
+}
